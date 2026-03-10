71a1621ee53a0e0f5d3885c95c4cb99a8a692f9f
Bobisan <boris.strinsky@protonmail.com>
2026-03-10 11:04:53 +0200

    Added some buttons and pages for learning

--- WebApp/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApp/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fe75ebafe6a9f3b9de66df5cba195d585004540")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4e5eafcde793b1a73faa512789c027b2eb229ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
