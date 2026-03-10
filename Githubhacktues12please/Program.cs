aa826af01c5221a11d1c39fea40440e48721d906
AdiLand_King <116757159+AdiLandKing@users.noreply.github.com>
2026-03-10 14:43:48 +0200

    test github

--- Githubhacktues12please/Program.cs
+++ Githubhacktues12please/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+//test for github hacktues 12 please
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
